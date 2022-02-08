8a6a70d09b62d0eefe3f74d49187cfe2a5a078a0
ErickSchuenck <esfmokdeci@gmail.com>
2022-02-08 19:27:02 -0300

    agora com tela cinzenta quando a sidebar abrir e tela de entrada e tela de chat conectadas

--- main.js
+++ main.js
@@ -5,12 +5,31 @@ function hide_lock_screen(){
     alert('Hello ' + my_name);
 
     // O código abaixo faz a tela inicial (lockscreen) desaparecer//
-
-    /*const lock_screen = document.querySelector('.lock_screen');
-    lock_screen.classList.add('.hide');
-    fiquei curioso para saber por quê isso não funcionou
-    */
-
     document.querySelector('.lock_screen').style.display = 'none';
+    show_loading();
+}
+function show_loading(){
     document.querySelector('.load_screen').style.display = 'block';
+    setTimeout(hide_loading, 3000);
+    setTimeout(show_chat_screen, 3000);
+}
+function hide_loading(){
+    document.querySelector('.load_screen').style.display = 'none';
+}
+function show_chat_screen(){
+    document.querySelector('.chat_screen').style.display = 'block';
+}
+function open_side_menu(){
+    document.querySelector('.sidebar').style.right = '0px';
+    display_gray_screen();
+}
+function close_side_menu(){
+    document.querySelector('.sidebar').style.right = '-250px';
+    hide_gray_screen();
+}
+function display_gray_screen(){
+    document.querySelector('.gray_screen').style.display = 'block';
+}
+function hide_gray_screen(){
+    document.querySelector('.gray_screen').style.display = 'none';
 }
