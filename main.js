ea73ca4559e523614ca38314e22de64c893411bb
ErickSchuenck <esfmokdeci@gmail.com>
2022-02-13 11:35:26 -0300

    mensagens agora aparecem na tela

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
 let my_name;
 let main = document.querySelector('.chat_container');
+
+let data = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages')
+    .then(extract_data)
+    .then(generate_new_messages);
+function extract_data(apiMessages){
+    console.log(apiMessages.data)
+    return apiMessages.data;
+}
+function generate_new_messages(data){
+    let messages = "";
+    for(let i = 0; i < data.length; i++){
+        messages += new_message(data[i]);
+    } 
+    let displayed_messages = document.querySelector('.chat_container');
+    displayed_messages.innerHTML += messages;
+}
+function entrance_signal(){
+    const dados = {name: my_name};
+    axios.post("https://mock-api.driven.com.br/api/v4/uol/participants",dados)
+    setInterval(status_signal, 5000); 
+}
+function status_signal(){
+    const dados = {name: my_name};
+    axios.post("https://mock-api.driven.com.br/api/v4/uol/status",dados);
+}
+function new_message(msg) {
+    let {from: user_name, text: user_message} = msg;
+    return `<div class="talk_baloon"><span>${user_name}</span> para <span>TODOS </span>: ${user_message}</div>`;
+}
 function hide_lock_screen(){
     my_name = document.getElementById("my_name").value;
     console.log('Hello ' + my_name);
@@ -17,7 +46,7 @@ function hide_loading(){
 }
 function show_chat_screen(){
     document.querySelector('.chat_screen').style.display = 'block';
-    entrance_signal();
+    entrance_signal(); 
 }
 function hide_chat_screen(){
     document.querySelector('.chat_screen').style.display = 'none';
@@ -36,13 +65,14 @@ function display_gray_screen(){
 function hide_gray_screen(){
     document.querySelector('.gray_screen').style.display = 'none';
 }
+/*
 function send_message(){
     let my_message = document.getElementById("my_message").value;
     if(my_message === ""){return}
     main.innerHTML = main.innerHTML + `
     <div class = 'talk_baloon'>${my_name} disse: ${my_message}</div>`
     document.getElementById('my_message').value = "";
-}
+} */
 function return_login_screen(){
     my_name = "";
     window.location.reload();
@@ -52,8 +82,9 @@ document.addEventListener("keypress", function(e){
         send_message();
     }
 })
+/*
 function entrance_signal(){
-    console.log("oi, eu estou funcionando");
     main.innerHTML = main.innerHTML + `
     <div class = 'talk_baloon'>${my_name} entrou na sala</div>`
 }
+*/
